18f98bafc24fe1abb3e1ae9b281a1d418463d20e
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-03-17 07:18:33 +0530

    updated

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = React.useState(0)
+
+  React.useEffect(() => {
+    const start = setInterval(() => {
+      setCount(prevNum => prevNum + 1)
+    }, 1000)
+    return () => clearInterval(start)
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>I'm counting {count} times</h1>
     </>
   )
 }
